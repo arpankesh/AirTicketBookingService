8e64f2232f38936cc8ae161e533ce4f0bd86d4e7
arpankesh <arpankesh1999@gmail.com>
2023-01-08 21:10:07 +0530

    Succcessfully sending a ticket payload to queue

--- src/controller/booking-controller.js
+++ src/controller/booking-controller.js
@@ -13,8 +13,16 @@ class BookingController {
 
     async sendMessageToQueue(req, res) {
         const channel = await createChannel();
-        const data = {message: 'Success'};
-        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+        const payload = {
+            data: {
+                subject: "This is a notification from a queue",
+                content: "Some service will subscribe to this queue",
+                recepientEmail: "airlinemanagementsanket@gmail.com",
+                notificationTime: "2023-01-08T20:50:00.000"
+            },
+            service: "CREATE_TICKET"
+        };
+        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(payload));
         return res.status(200).json({
             message: "Suucessfully published the event"
         });
